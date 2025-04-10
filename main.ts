2566a2ccc6d2d13856bbc27373cd5946dcde98a1
hourfrozen <hourfrozen4@gmail.com>
2025-04-10 10:10:58 -0400

    a

--- main.ts
+++ main.ts
@@ -1,17 +1,6 @@
 // @deno-types="npm:@types/express@4.17.15"
 import express from "npm:express";
-const port = 8080; //https
-import https from "node:https";
-import { readFileSync } from "node:fs";
-import { resolve } from "node:path";
-import { homedir } from "node:os";
-const certPath = resolve(homedir(), "Desktop/=key/cert.pem");
-const keyPath = resolve(homedir(), "Desktop/=key/key.pem");
 
-const options = {
-  key: readFileSync(keyPath),
-  cert: readFileSync(certPath),
-}
 const app = express();
 import api from "./api.ts";
 
@@ -21,6 +10,5 @@ app.get("/", (_req, res) => {
   // res.sendFile(`../pages/landing.html`);
 });
 
-https.createServer(options, app).listen(port, () => {
-  console.log("Secure Express server running on port 8080");
-});
+app.listen(8080);
+console.log("monorouter software online");
