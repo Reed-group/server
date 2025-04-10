4bf85f57fbb79084225210db91b969629ba82778
hourfrozen <hourfrozen4@gmail.com>
2025-04-10 16:57:35 -0400

    security: end-to-end encryption. designed for the webserver

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 // @deno-types="npm:@types/express@4.17.15"
 import express from "npm:express";
-
-const app = express();
 import api from "./api.ts";
+import fs from "node:fs";
+import path from "node:path";
+import os from "node:os";
+import https from "node:https";
+const app = express();
+
+const combcert = fs.readFileSync(path.resolve(os.homedir(), "Desktop", "=key", "certcf.pem"));
+const webserver = https.createServer({
+  cert: combcert,
+  key: combcert,
+}, app)
 
 app.use("/api", api);
 app.get("/", (_req, res) => {
@@ -10,5 +19,5 @@ app.get("/", (_req, res) => {
   // res.sendFile(`../pages/landing.html`);
 });
 
-app.listen(8080);
+webserver.listen(8080);
 console.log("monorouter software online");
