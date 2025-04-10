20fb4e5bf9568ca6d59913f709a8dca6f9948925
hourfrozen <hourfrozen4@gmail.com>
2025-04-09 21:55:51 -0400

    reedserv secure server configuration

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
 // @deno-types="npm:@types/express@4.17.15"
 import express from "npm:express";
 const port = 8080; //https
+import https from "node:https";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { homedir } from "node:os";
+const certPath = resolve(homedir(), "Desktop/=key/cert.pem");
+const keyPath = resolve(homedir(), "Desktop/=key/key.pem");
+
+const options = {
+  key: readFileSync(keyPath),
+  cert: readFileSync(certPath),
+}
 const app = express();
 import api from "./api.ts";
 
@@ -10,6 +21,6 @@ app.get("/", (_req, res) => {
   // res.sendFile(`../pages/landing.html`);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+https.createServer(options, app).listen(port, () => {
+  console.log("Secure Express server running on port 8080");
 });
